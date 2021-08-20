e9f12e671018f311d90bd72cf12194654ff0a037
Amit Halle <amit.halle@sysaid.com>
2021-08-20 14:10:19 +0300

    add import from xml logic

--- src/server/xml_handler.py
+++ src/server/xml_handler.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as ET
+from . import get_song, word_groups, create_song, phrases, LoadFile
+
 SONG = "song"
 PHRASE = "phrase"
 WORD_GROUP = "word_group"
@@ -46,5 +48,47 @@ def row2dict(row):
 
 def to_string(element):
     s = ET.tostring(element, encoding="unicode")
-
     return s
+
+
+def import_song(song_xml):
+    author = song_xml.get("author")
+    name = song_xml.get("song_name")
+    create_date = song_xml.get("create_date")
+    song_id = create_song.insert_song_metadata(name, author, create_date)
+
+    for word in song_xml.getchildren():
+        import_word_in_song(word, song_id)
+    return song_id
+
+
+def import_word_in_song(word_xml, song_id):
+    word = word_xml.get("word")
+    stanzaindex = word_xml.get("stanzaindex")
+    lineindex = word_xml.get("lineindex")
+    lineGlobalindex = word_xml.get("lineGlobalindex")
+    wordindex = word_xml.get("wordindex")
+    LoadFile.insert_word_in_song(song_id, word, stanzaindex, lineGlobalindex, lineindex, wordindex)
+
+def import_phrase(phrase_xml):
+    phrase = phrase_xml.get("phrase")
+    phrases.create_phrase(phrase)
+
+
+def import_word_group(word_group_xml):
+    group_name = word_group_xml.get("group_name")
+    words = [word.text for word in word_group_xml.find("words").getchildren()]
+    word_groups.create_word_group(group_name, words)
+
+def read_imported_file(filepath):
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+    for child in root.getchildren():
+        if child.tag == SONG:
+            import_song(child)
+        elif child.tag == PHRASE:
+            import_phrase(child)
+        elif child.tag == WORD_GROUP:
+            import_word_group(child)
+        else:
+            raise Exception("tag is not suppprted:" + child.tag)
